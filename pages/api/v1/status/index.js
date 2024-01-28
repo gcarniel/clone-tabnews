98ea5cd2216a6db8652cc2918f18725ab72fd3ce
Gabriel Carniel <gpcarniel@outlook.com>
2024-01-28 12:12:56 -0300

    add `database` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,8 @@
-export default function handler(req, res) {
+
+import database from "../../../../infra/database"
+
+export default async function handler(req, res) {
+  const resp = await database.query("SELECT 1 as num")
+  console.log(resp.rows)
   res.status(200).json({ status: "ok" })
 }
