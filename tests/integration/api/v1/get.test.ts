f5ec5ecdbbcc95014edf0e157e99a4cb1e289a1b
Gabriel Carniel <gpcarniel@outlook.com>
2024-03-26 22:40:35 -0300

    api status with data to database and tests

--- tests/integration/api/v1/get.test.ts
+++ tests/integration/api/v1/get.test.ts
@@ -1,7 +1,13 @@
 
-test("GET to /api/v1/status should return status 200 and { status: 'ok' }", async () => {
+test("GET to /api/v1/status should return status 200 and correct data", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status")
   expect(response.status).toBe(200)
-  const json = await response.json()
-  expect(json.status).toBe("ok")
+  const responseBody = await response.json()
+
+  const updatedAt = new Date(responseBody.updated_at).toISOString()
+  expect(responseBody.updated_at).toEqual(updatedAt)
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0")
+  expect(responseBody.dependencies.database.max_connections).toEqual(100)
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1)
 })
