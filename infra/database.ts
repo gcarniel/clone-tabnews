495037559c4fa5aa168af7d3cafebbbe2bd3bd96
Gabriel Carniel <gpcarniel@outlook.com>
2024-04-10 22:36:49 -0300

    add ssl to connection

--- infra/database.ts
+++ infra/database.ts
@@ -6,7 +6,8 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    port:parseInt(process.env.POSTGRES_PORT)
+    port:parseInt(process.env.POSTGRES_PORT),
+    ssl: process.env.NODE_ENV === "development" ? false : true
   });
 
 try {
